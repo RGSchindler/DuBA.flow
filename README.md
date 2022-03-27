f7a33b4cee7c5efffb80afe52c3a3ef6064501a5
Cedric K. Brinkmann <56127742+CedKB@users.noreply.github.com>
2022-03-27 20:55:13 +0200

    refactorization

--- README.md
+++ README.md
@@ -2,35 +2,39 @@
 Software pipeline for nanopore sequencing
 
 ## Usage
-This program was tested on Ubuntu 20.04 with following installations:
+This program was tested on Ubuntu 20.04 & Python ver. 3.8.1 with following packages:
 
 Tool | Version | Installation 
 -----|---------|-------------
-pyYAML | 6.0 | pip install pyYAML
-samtools | 1.9 | conda install -c bioconda samtools=1.9
-minimap2 | 2.24 | conda install -c bioconda minimap=2.24
-ngmlr | 0.2.7 | conda install -c bioconda ngmlr=0.2.7
-canu | 1.9 | conda install -c bioconda canu=1.9
-longshot | 0.4.1 | conda install -c bioconda longshot=0.4.1
-sniffles | 1.0.12 | conda install -c bioconda sniffles=1.0.12
-NanoPlot | 1.39.0 | pip install NanoPlots
+[pyYAML](https://github.com/yaml/pyyaml) | 6.0 | pip install pyYAML
+[samtools](https://github.com/samtools/samtools) | 1.9 | conda install -c bioconda samtools=1.9
+[minimap2](https://github.com/lh3/minimap2) | 2.24 | conda install -c bioconda minimap2=2.24
+[ngmlr](https://github.com/philres/ngmlr) | 0.2.7 | conda install -c bioconda ngmlr=0.2.7
+[canu](https://github.com/marbl/canu) | 1.9 | conda install -c bioconda canu=1.9
+[longshot](https://github.com/pjedge/longshot) | 0.4.1 | conda install -c bioconda longshot=0.4.1
+[Sniffles](https://github.com/fritzsedlazeck/Sniffles) | 1.0.12 | conda install -c bioconda sniffles=1.0.12
+[NanoPlot](https://github.com/wdecoster/NanoPlot) | 1.39.0 | pip install NanoPlots
+[minibar](https://github.com/calacademy-research/minibar) | 1.0 | GitHub download & install
 
 
 ### Command Line Options
-Argument | Required | Type | Default | Help 
----------|----------|------|---------|------
-\-conf | True | string | config.yml | Config file (yml)
-\-ref | False | string | reference_map.yml | Reference map (yml)
+Argument | Required | Type | Default | Example |Help 
+---------|----------|------|---------|---------|----
+\-conf | True | string | config.yml | -conf config.yml |Config file (yml)
+\-all | True | Flag | False | -all | Set this flag to run the complete pipeline
+\-s | True | string | None | -s 1234 | Only run parts of the pipeline, i.e.:  <ul> 1. create concat fasta <br /> 2. run minibar <br /> 3. create working directories <br /> 4. run faidx <br /> 5. run nanoplot <br /> 6. run minimap 2 pipeline <br /> 7. run canu pipeline </ul>
+
+Note:   
+Either -all or -s must be set in order to run the pipeline (mutually exclusive).
 
 Example:   
-> python pipeline.py -conf config.yml -ref reference_map.yml
+> python pipeline.py -conf config.yml -all    
+> python pipeline.py -conf config.yml -s 1234
 
 ## Known Issues
 
 ## To be Implemented
-- Arguments to start and stop at specific steps
 - Multithread minimap/ngmlr to speed up the procedure
-- Implement the minibar step
 - Add on downstream analysis
 
 ## References
--- pipeline.py
+++ pipeline.py
@@ -1,21 +1,25 @@
 import os
+import re
 import argparse
+import shutil
 from subprocess import Popen, PIPE
 
 import yaml
-
 
 
 ###########################################################################################
 ########################          Argument Parser             #############################
 ###########################################################################################
 
-parser = argparse.ArgumentParser(description="Nanopore Sequencing Pipeline", formatter_class=argparse.RawTextHelpFormatter)
+parser = argparse.ArgumentParser(
+    description="Nanopore Sequencing Pipeline", formatter_class=argparse.RawTextHelpFormatter
+)
 
 ## ADD Arguments
 parser.add_argument("-conf", default="config.yml", required=True, type=str, help="Config file (yaml)")
-parser.add_argument("-ref", default="reference_map.yml", type=str, help="Map of references (yaml)")
-
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-all", action="store_true", help="Set flag to run the whole pipeline")
+group.add_argument("-s", type=int, help="Run seperate parts of the pipeline")
 
 args = parser.parse_args()
 
@@ -24,35 +28,76 @@ args = parser.parse_args()
 ########################          Auxilary Function           #############################
 ###########################################################################################
 
-def create_working_dicts(idx):
-    os.mkdir(os.path.join(path, "00_analysis"))
-    os.mkdir(os.path.join(path, "00_analysis/NanoPlot"))
-    os.mkdir(os.path.join(path, "00_analysis/canu"))
-    os.mkdir(os.path.join(path, "00_analysis/minimap2"))
-    os.mkdir(os.path.join(path, "00_analysis/ngmlr"))
 
-    for i in range(1, idx+1):
-        os.mkdir(os.path.join(path, f"00_analysis/canu/barcode{'{0:03}'.format(i)}"))
-        os.mkdir(os.path.join(path, f"00_analysis/minimap2/barcode{'{0:03}'.format(i)}"))
-        os.mkdir(os.path.join(path, f"00_analysis/ngmlr/barcode{'{0:03}'.format(i)}"))
+def create_concat_fasta(non_demultiplexed, concat_path):
+    files = os.listdir(non_demultiplexed)
 
-        
-def run_faidx(refs):
-    for ref in refs:
-        process = Popen(["samtools", "faidx", ref], stdout=PIPE, stderr=PIPE)
+    with open(concat_path, "wb") as fd:
+        for file in files:
+            with open(file, "rb") as fs:
+                shutil.copyfileobj(fs, fd)
+
+
+def run_minibar(path, barcode_file, concat_path):
+
+    cmd = f'python minibar.py -T -F {barcode_file} {concat_path} -P ""'
+
+    process = Popen(args=cmd, stdout=PIPE, stderr=PIPE, shell=True)
+
+    stdout, stderr = process.communicate()
+    if stderr:
+        print(stderr.decode("ascii"))
+
+    for file in os.listdir():
+        if file.split(".")[-1] == "fastq":
+            if file.count("_") == 1:
+                shutil.move(file, f"{path}/{ONTrun}{file}")
+            else:
+                shutil.move(
+                    file,
+                    f"{path}/misc/{ONTrun}{file}",
+                )
+
+
+def mkdir(folder_path):
+    # Check if the folders already exists
+    if not os.path.isdir(folder_path):
+        os.mkdir(folder_path)
+
+
+def create_working_dirs(idx):
+    mkdir(os.path.join(path, "00_analysis"))
+    mkdir(os.path.join(path, "00_analysis/NanoPlot"))
+    mkdir(os.path.join(path, "00_analysis/canu"))
+    mkdir(os.path.join(path, "00_analysis/minimap2"))
+    mkdir(os.path.join(path, "00_analysis/ngmlr"))
+
+    for i in idx:
+        mkdir(os.path.join(path, f"00_analysis/canu/barcode{i}"))
+        mkdir(os.path.join(path, f"00_analysis/minimap2/barcode{i}"))
+        mkdir(os.path.join(path, f"00_analysis/ngmlr/barcode{i}"))
+
+
+# Only run on unique
+def run_faidx(run_ids):
+    for rid in run_ids:
+        # print(references[rid])
+        process = Popen(["samtools", "faidx", references[rid]], stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
-        if stderr: print(stderr)
+        print(stdout.decode("ascii"))
+        if stderr:
+            print(stderr.decode("ascii"))
 
 
 def run_nanoplot():
     cmd = f"NanoPlot --summary sequencing_summary.txt --loglength -o {path}/00_analysis/NanoPlot"
 
-    process = Popen(args=cmd.split(" "),
-                    stdout=PIPE, stderr=PIPE)
+    process = Popen(args=cmd.split(" "), stdout=PIPE, stderr=PIPE)
 
     stdout, stderr = process.communicate()
 
-    if stderr: print(stderr)
+    if stderr:
+        print(stderr.decode("ascii"))
 
 
 def run_minimap2(ref, barcode, idx, print_out=False):
@@ -64,17 +109,18 @@ def run_minimap2(ref, barcode, idx, print_out=False):
         f"samtools index {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_sort.bam",
         f"samtools stats {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_sort.bam | grep '^SN' | cut -f 2- > {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_map.txt",
         f"samtools idxstats {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_sort.bam > {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}-reads-chr.txt",
-        f"samtools depth {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_sort.bam > {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}.coverage"
-
+        f"samtools depth {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_sort.bam > {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}.coverage",
     ]
 
     for c in cmds:
-        process = Popen(args=c,
-   	                stdout=PIPE, stderr=PIPE, shell=True)
+        # print(c, "\n")
+        process = Popen(args=c, stdout=PIPE, stderr=PIPE, shell=True)
 
         stdout, stderr = process.communicate()
-        if stderr: print(stderr, "\n")
-        #if print_out: print(stdout)
+        if stderr:
+            print(stderr.decode("ascii"))
+        if print_out:
+            print(stdout)
 
 
 def run_ngmlr(ref, barcode, idx, print_out=False):
@@ -86,45 +132,53 @@ def run_ngmlr(ref, barcode, idx, print_out=False):
         f"samtools depth {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr_sort.bam > {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr.coverage",
         f"samtools stats {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr_sort.bam | grep '^SN' | cut -f 2- > {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}-reads-chr.txt",
         f"sniffles -m {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr_sort.bam -b {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr-10.bedpe -t 8",
-        f"sniffles -m {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr_sort.bam -s 5 -b {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr-5.bedpe -t 8"
+        f"sniffles -m {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr_sort.bam -s 5 -b {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr-5.bedpe -t 8",
     ]
 
     for c in cmds:
-        process = Popen(args=c,
-                    stdout=PIPE, stderr=PIPE, shell=True)
+        process = Popen(args=c, stdout=PIPE, stderr=PIPE, shell=True)
 
         stdout, stderr = process.communicate()
-        if stderr: print(stderr.decode("ascii"))
-        if print_out: print(stdout)
-
+        if stderr:
+            print(stderr.decode("ascii"))
+        if print_out:
+            print(stdout)
 
 
 def run_longshot(ref, idx, mapper, E=0.5):
     if mapper == "minimap2":
-        cmd = f"longshot --bam {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_sort.bam --ref {ref} --out {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_longshot.vcf -E {E}"
+        cmd = f"longshot --bam {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_sort.bam --ref {ref} --out {pathMinimap2}/barcode{idx}/{ONTrun}S{idx}_longshot.vcf"
     elif mapper == "ngmlr":
-        cmd = f"longshot --bam {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr_sort.bam --ref {ref} --out {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_longshot.vcf -E {E}"    
+        cmd = f"longshot --bam {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_ngmlr_sort.bam --ref {ref} --out {pathNgmlr}/barcode{idx}/{ONTrun}S{idx}_longshot.vcf"
     else:
-        print("ERROR: Please specify a correct mapper!")
-        return None
-    
-    process = Popen(args=cmd.split(" "), 
-                    stdout=PIPE, stderr=PIPE)
-    
-    stdout, stderr = process.communicate()
-    if stderr: print(stderr.decode("ascii"))
+        raise ("ERROR: Please specify a correct mapper!")
 
-        
+    # print(cmd, "\n")
+    process = Popen(args=cmd.split(" "), stdout=PIPE, stderr=PIPE)
+
+    stdout, stderr = process.communicate()
+    if stderr:
+        print(stderr.decode("ascii"))
+
+
 def run_canu(barcode, genome, idx):
     cmd = f"canu -p barcode{idx} -d {pathCanu}/barcode{idx} genomeSize={genome} -nanopore-raw {barcode} correctedErrorRate=0.14 stopOnLowCoverage=0"
-    
-    process = Popen(args=cmd.split(" "), 
-                    stdout=PIPE, stderr=PIPE)
-    
-    stdout, stderr = process.communicate()
-    if stderr: print(stderr)
 
-      
+    print(cmd)
+    process = Popen(args=cmd, stdout=PIPE, stderr=PIPE, shell=True)
+
+    stdout, stderr = process.communicate()
+
+    print(stdout)
+    if stderr:
+        print(stderr.decode("ascii"))
+
+    """
+    print(stdout.decode("ascii"))
+    if stderr: print(stderr.decode("ascii"))
+    """
+
+
 ###########################################################################################
 ########################             Configuration            #############################
 ###########################################################################################
@@ -132,13 +186,16 @@ def run_canu(barcode, genome, idx):
 
 with open(args.conf) as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-    #print(config)
+    # print(config)
 
 
 path = config["path"]
 ONTrun = config["ONTrun"]
 pathRef = config["pathRef"]
-num_items = config["sampleNumber"]
+references_map = config["refMap"]
+non_demultiplexed = config["nonDemultiplexed"]
+bc_file = config["barcodeFile"]
+concat_fasta_path = config["concat_fasta_path"]
 
 pathFastq = f"{path}{ONTrun}{config['pathFastq']}"
 pathBarcode = f"{pathFastq}{config['pathBarcode']}"
@@ -151,47 +208,87 @@ pathNgmlr = f"{path}{config['pathNgmlr']}"
 ########################             Referene Map             #############################
 ###########################################################################################
 
-with open(args.ref) as f:
+with open(references_map) as f:
     refmap = yaml.load(f, Loader=yaml.FullLoader)
 
 ref_files = list(refmap["references"].values())
 references = [f"{pathRef}/{r}" for r in ref_files]
 
+"""
 if len(references) != num_items:
-    print("ERROR: Number of references and barcodes is not the same, make suer the reference_map.yaml file is ") 
+    print("ERROR: Number of references and barcodes is not the same, make sure the reference_map.yaml file is ") 
+"""
 
 for ref in references:
-    if not os.path.isfile(ref): 
-        print(f"ERROR: Reference file {ref} dosen't exist!")
+    if not os.path.isfile(ref):
+        raise(f"ERROR: Reference file {ref} dosen't exist!")
 
 
+# Check demultiplex output:
+regex = r"(?!.*\_.*)(?<=S).*(?=\.)"  # get the running number from file_name
+run_ids = []
+for file_name in os.listdir(path):
+    id_buffer = re.findall(regex, file_name, re.MULTILINE)
+    if id_buffer:
+        run_ids.extend(id_buffer)
 
-genome = ["15k"]*num_items
-barcode = [f"{path}{ONTrun}_S{'{0:03}'.format(i)}.fastq" for i in range(1, num_items+1)]
+
+genome = ["15k"] * len(run_ids)
+barcode = [f"{path}{ONTrun}S{i}.fastq" for i in run_ids]
+
+barcode = {}
+references = {}
+for rid in run_ids:
+    r_buffer = refmap["references"][f"ref{rid}"]
+
+    barcode[rid] = f"{path}{ONTrun}S{rid}.fastq"
+    references[rid] = f"{pathRef}/{r_buffer}"
+
+# barcode = sorted(buffer_barcode.items())
+# references = sorted(buffer_references.items())
 
 
 ###########################################################################################
 ########################              Main Loop               #############################
 ###########################################################################################
 
+def minimap_pipe():
+    for rid in run_ids:
+        run_minimap2(ref=references[rid], barcode=barcode[rid], idx=rid, print_out=False)
+        run_longshot(ref=references[rid], idx=rid, mapper="minimap2", E=0.5)
 
 
-create_working_dicts(idx=num_items)
-
-run_faidx(refs=references)
-
-run_nanoplot()
-
-
-for i in range(num_items):
-    # minimap2 LOOP!
-    run_minimap2(ref=references[i], barcode=barcode[i], idx="{0:03}".format(i+1), print_out=False)
-    run_longshot(ref=references[i], idx="{0:03}".format(i+1), mapper="minimap2", E=0.5)
+def canu_pipe():
+    for rid in run_ids:
+        run_ngmlr(ref=references[rid], barcode=barcode[rid], idx=rid, print_out=False)
+        run_longshot(ref=references[rid], idx=rid, mapper="ngmlr", E=0.5)
+        run_canu(barcode=barcode[rid], genome="15k", idx=rid)
 
 
-for i in range(num_items):
-    # ngmlr
-    run_ngmlr(ref=references[i], barcode=barcode[i], idx="{0:03}".format(i+1), print_out=False)
-    run_longshot(ref=references[i], idx="{0:03}".format(i+1), mapper="ngmlr", E=0.5)
-    run_canu(barcode=barcode[i], genome=genome[i], idx="{0:03}".format(i+1))
+if concat_fasta_path:
+    concat_path = concat_fasta_path
+else:
+    concat_path = f"{non_demultiplexed}{ONTrun}_ConcatFasta.fastq.gz"
 
+
+pipeline = {
+    "1": create_concat_fasta(non_demultiplexed=non_demultiplexed, concat_path=concat_path),
+    "2": run_minibar(path=path, barcode_file=bc_file, concat_path=concat_path),
+    "3": create_working_dirs(idx=run_ids),
+    "4": run_faidx(run_ids),
+    "5": run_nanoplot(),
+    "6": minimap_pipe(),
+    "7": canu_pipe(),
+}
+
+if args.all:
+    for i in range(1, 8):
+        pipeline[str(i)]
+
+elif args.s:
+    sep_pipe = [s for s in args.s]
+    for sep in sep_pipe:
+        pipeline[sep]
+
+else:
+    raise ("ERROR, neither --all nor --s was set")
